eb38535a0ce0dece3305c7de47888f8901a6a832
Richard Walker <richard_w10@hotmail.com>
2019-08-21 20:25:58 +0100

    react

---
+++ src/redux/configureStore.js
@@ -0,0 +1,6 @@
+//CommonJS for dynamic import during build time
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./configureStore.prod");
+} else {
+  module.exports = require("./configureStore.dev");
+}
